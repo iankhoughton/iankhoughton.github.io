8f23f2c9f012b1ac3962b754d915cc27fea15daa
Ian Houghton <ianhoughton@gmail.com>
2024-02-01 08:50:19 -0800

    Create gettingHere.js

---
+++ gettingHere.js
@@ -0,0 +1,11 @@
+var wet_boew_geomap = {
+	basemap: {
+		title: "MapQuest OSM Map",
+		type: "xyz",
+		mapOptions: {
+			projection: "EPSG:900913",
+			center: [-90.7222765, 62.5410882],
+			zoomLevel: 3
+		}
+	}
+};
